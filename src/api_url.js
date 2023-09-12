3da8da1e59ef1bf01949a39690e6b806b1d0c5dc
Raman Madhukar <71077580+RamanMadhukar@users.noreply.github.com>
2023-09-12 11:59:15 +0530

    api change

--- src/api_url.js
+++ src/api_url.js
@@ -1,6 +1,6 @@
 // const BASE_URL = "http://localhost:5000/api";
-const BASE_URL = "https://whale-app-rqcs6.ondigitalocean.app/api";
-// const BASE_URL = "https://wipro-backend.onrender.com/api";
+// const BASE_URL = "https://whale-app-rqcs6.ondigitalocean.app/api";
+const BASE_URL = "https://wipro-backend.onrender.com/api";
 export default BASE_URL;
 
 //https://stingray-app-myegm.ondigitalocean.app/api
